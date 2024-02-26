6a350a2ae17dd6dca63bd29e5de29f298eb28dad
yuma14 <yuma140902@gmail.com>
2024-02-27 03:34:39 +0900

    feat: layout

--- layouts/LayoutDefault.tsx
+++ layouts/LayoutDefault.tsx
@@ -3,6 +3,24 @@ import './tailwind.css';
 import React from 'react';
 import { Link } from '../components/Link';
 
+// TODO: tmp
+function Lorem() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim
+      labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet.
+      Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum
+      Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident.
+      Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex
+      occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat
+      officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in
+      Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non
+      excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco
+      ut ea consectetur et est culpa et culpa duis.
+    </p>
+  );
+}
+
 export default function LayoutDefault({
   children,
 }: {
@@ -11,11 +29,14 @@ export default function LayoutDefault({
   return (
     <div className="min-h-screen font-sans">
       <Header />
-      <div className="flex flex-nowrap flex-col lg:flex-row-reverse">
-        <div className="flex-grow">
+      <div className="flex flex-nowrap flex-col lg:flex-row-reverse lg:h-screen lg:fixed lg:top-0 lg:left-0 lg:right-0">
+        <div className="flex-grow lg:overflow-auto pt-12">
           <Content>{children}</Content>
         </div>
-        <div className="flex-grow-0">
+        <div
+          id="sidebar"
+          className="flex-grow-0 flex-shrink-0 lg:overflow-auto lg:mt-12"
+        >
           <Sidebar />
         </div>
       </div>
@@ -24,16 +45,28 @@ export default function LayoutDefault({
 }
 
 function Content({ children }: { children: React.ReactNode }) {
-  return <div className="max-w-5xl mx-auto top-12">{children}</div>;
+  return <div className="max-w-5xl mx-auto">{children}</div>;
 }
 
 function Sidebar() {
-  return <div className="bg-red-100 w-full lg:w-52 lg:h-full">sidebar</div>;
+  return (
+    <div className="bg-red-100 w-full lg:w-52 lg:min-h-full">
+      BEGINNING OF SIDEBAR
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      <Lorem />
+      END OF SIDEBAR
+    </div>
+  );
 }
 
 function Header() {
   return (
-    <header className="bg-header-color backdrop-blur-sm border-b border-solid border-header-border-color text-white h-12 sticky top-0 w-full">
+    <header className="z-50 bg-header-color backdrop-blur-sm border-b border-solid border-header-border-color text-white h-12 fixed top-0 w-full">
       <div className="flex items-center h-full">
         <div className="px-5">yuma14.net</div>
       </div>
--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -40,6 +40,7 @@ export default function Page() {
       <Lorem />
       <Lorem />
       <Lorem />
+      END OF CONTENT
     </>
   );
 }
