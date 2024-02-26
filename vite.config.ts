a13e1dedf67ffdd306d148b73c8b0507f247db23
yuma14 <yuma140902@gmail.com>
2024-02-27 00:34:52 +0900

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import ssr from "vike/plugin";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import ssr from 'vike/plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
   plugins: [react({}), ssr({})],
 });
