a5be32611a1ac99851b4492eed62366b06c54fa5
yuma14 <yuma140902@gmail.com>
2024-02-27 02:50:59 +0900

    feat: header

--- layouts/LayoutDefault.tsx
+++ layouts/LayoutDefault.tsx
@@ -1,3 +1,4 @@
+import './variables.css';
 import './tailwind.css';
 import React from 'react';
 import { Link } from '../components/Link';
@@ -8,12 +9,23 @@ export default function LayoutDefault({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen font-sans">
+      <Header />
       <Content>{children}</Content>
     </div>
   );
 }
 
 function Content({ children }: { children: React.ReactNode }) {
-  return <div className="max-w-5xl mx-auto">{children}</div>;
+  return <div className="max-w-5xl mx-auto top-12">{children}</div>;
+}
+
+function Header() {
+  return (
+    <header className="bg-header-color backdrop-blur-sm border-b border-solid border-header-border-color text-white h-12 sticky top-0 w-full">
+      <div className="flex items-center h-full">
+        <div className="px-5">yuma14.net</div>
+      </div>
+    </header>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,16 @@ import daisyui from 'daisyui';
 export default {
   content: ['./{pages,layouts,components,src}/**/*.{html,js,jsx,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'header-color':
+          'color-mix(in srgb, var(--color-primary-500) 30%, transparent)',
+      },
+      borderColor: {
+        'header-border-color':
+          'color-mix(in srgb, var(--color-primary-500) 45%, transparent)',
+      },
+    },
   },
   plugins: [daisyui],
 } satisfies Config;
