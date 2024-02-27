20ea6c91898ab6920209ae7ceed3bc6642b0d561
yuma14 <yuma140902@gmail.com>
2024-02-28 00:41:37 +0900

    feat: add favicon and setup ogp

--- layouts/HeadDefault.tsx
+++ layouts/HeadDefault.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import logoUrl from '../assets/logo.svg';
-
-// Default <head> (can be overridden by pages)
 
 export default function HeadDefault() {
   return (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content="Demo showcasing Vike" />
-      <link rel="icon" href={logoUrl} />
+      <meta property="og:image" content="/ogp.png" />
+      <meta property="og:title" content="yuma14.net" />
+      <meta property="og:locale" content="ja_JP" />
+      <meta property="og:description" content="yuma14の個人サイト" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/icon-512x512.png" />
     </>
   );
 }
