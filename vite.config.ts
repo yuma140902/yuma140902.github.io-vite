3b1643759082465ebd5afa0d6433fb582e57b4cd
yuma14 <yuma140902@gmail.com>
2024-02-27 22:44:03 +0900

    chore: remove yaml plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import vike from 'vike/plugin';
 import react from '@vitejs/plugin-react';
-import YAMLPlugin from 'unplugin-yaml/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
-  plugins: [react({}), vike({ prerender: true }), YAMLPlugin({})],
+  plugins: [react({}), vike({ prerender: true })],
 });
