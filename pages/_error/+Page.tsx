5b8bb52626da525b4b1dd46795aa4f65234838fe
yuma14 <yuma140902@gmail.com>
2024-02-28 00:28:48 +0900

    feat: pretty 404 page

--- pages/_error/+Page.tsx
+++ pages/_error/+Page.tsx
@@ -1,21 +1,22 @@
-import React from "react";
+import React from 'react';
 import { usePageContext } from 'vike-react/usePageContext';
+import { Article } from '../../components/Article';
 
 export default function Page() {
   const { is404 } = usePageContext();
   if (is404) {
     return (
-      <>
+      <Article>
         <h1>404 Page Not Found</h1>
         <p>This page could not be found.</p>
-      </>
+      </Article>
     );
   } else {
     return (
-      <>
+      <Article>
         <h1>500 Internal Server Error</h1>
         <p>Something went wrong.</p>
-      </>
+      </Article>
     );
   }
 }
