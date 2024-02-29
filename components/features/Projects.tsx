8f76f1654780c7020f12597f7713780a038646ff
yuma14 <yuma140902@gmail.com>
2024-02-29 21:18:10 +0900

    feat: add github link

--- components/features/Projects.tsx
+++ components/features/Projects.tsx
@@ -1,11 +1,26 @@
 import { ReactNode } from 'react';
+import { Tooltip } from 'react-daisyui';
 
 import { ProjectType, main_projects } from '@/src/projects';
 
-function Card(props: { head: ReactNode; children: ReactNode }) {
+function Card(props: { head: ReactNode; children: ReactNode; repo?: string }) {
   return (
     <div className="rounded border-solid border-base-tm-200 border hover:border-primary-tm flex-grow flex-shrink basis-full md:basis-1/3">
-      <div className="p-card-space bg-base-tm-150 font-bold text-lg border-b border-b-base-tm-200">{props.head}</div>
+      <div className="p-card-space bg-base-tm-150 border-b border-b-base-tm-200">
+        <div className="flex flex-row justify-stretch items-stretch">
+          <div className="font-bold text-lg">{props.head}</div>
+          <div className="flex-grow" />
+          <div className="flex items-center">
+            {props.repo ? (
+              <Tooltip position="left" message="GitHub" className="w-5 h-5">
+                <a href={props.repo} target="_blank" rel="noopener noreferrer" className="w-5 h-5">
+                  <span className="i-carbon-logo-github w-5 h-5" />
+                </a>
+              </Tooltip>
+            ) : undefined}
+          </div>
+        </div>
+      </div>
       <div className="p-card-space">{props.children}</div>
     </div>
   );
@@ -26,7 +41,7 @@ function Image(props: ProjectType) {
 
 function Project({ project }: { project: ProjectType }) {
   return (
-    <Card head={<div>{project.name}</div>}>
+    <Card head={project.name} repo={project.repo}>
       <div className="hero">
         <Image {...project} />
         {project.description}
