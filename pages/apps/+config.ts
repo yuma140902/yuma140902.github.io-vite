160eac90670b4735eb894bbec9a058c09a07aa98
yuma14 <yuma140902@gmail.com>
2024-02-29 19:50:02 +0900

    feat: use LayoutWide in /apps

--- pages/apps/+config.ts
+++ pages/apps/+config.ts
@@ -1,5 +1,8 @@
 import { Config } from 'vike/types';
 
+import { LayoutWide } from '@/layouts/Layouts';
+
 export default {
+  Layout: LayoutWide,
   title: 'ウェブアプリ一覧 - yuma14.net',
 } satisfies Config;
