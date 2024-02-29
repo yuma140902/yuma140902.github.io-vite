3fd4b402f0d41f8776e33c711a864cd3c7cda4be
yuma14 <yuma140902@gmail.com>
2024-02-29 13:18:09 +0900

    fix: prose width

--- components/Article.tsx
+++ components/Article.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 export function Article({ children }: { children: ReactNode }) {
   return (
-    <article className="prose prose-headings:border-b prose-headings:border-b-primary-tm">
+    <article className="prose max-w-none prose-headings:border-b prose-headings:border-b-primary-tm prose-headings:max-w-[50%]">
       {children}
     </article>
   );
