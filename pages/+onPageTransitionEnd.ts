f41b2c9dceaf3922362c7cb7e2f892d94804e7e9
yuma14 <yuma140902@gmail.com>
2024-03-19 02:02:02 +0900

    fix: ページ遷移後に必ずページの先頭にスクロールする

--- pages/+onPageTransitionEnd.ts
+++ pages/+onPageTransitionEnd.ts
@@ -2,4 +2,5 @@ import type { OnPageTransitionEndAsync } from 'vike/types';
 
 export const onPageTransitionEnd: OnPageTransitionEndAsync = async () => {
   document.querySelector('body')!.classList.remove('page-is-transitioning');
+  document.querySelector('main')?.scrollTo(0, 0);
 };
