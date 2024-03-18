29c46ac9a8017159542bc3b0a12b289570fb755a
yuma14 <yuma140902@gmail.com>
2024-03-19 00:17:17 +0900

    feat: リダイレクトを設定

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,17 @@ export default defineConfig({
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
-  plugins: [react({}), vike({ prerender: true })],
+  plugins: [
+    react({}),
+    vike({
+      prerender: true,
+      redirects: {
+        '/webtools/csv2tex.html': 'http://old.yuma14.net/webtools/csv2tex.html',
+        '/webtools/diff.html': 'http://old.yuma14.net/webtools/diff.html',
+        '/webtools/sort.html': 'http://old.yuma14.net/webtools/sort.html',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
