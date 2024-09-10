4deb056203bb815b2ad94d907155199e9878bb1c
yuma14 <yuma140902@gmail.com>
2024-09-11 01:57:17 +0900

    feat: add /apps/

---
+++ src/data/apps.ts
@@ -0,0 +1,99 @@
+import type { ImageMetadata } from "astro";
+import imgWebImageEditor from "../images/web-image-editor.png";
+import imgDiffTool from "../images/difftool-icon-512x512.png";
+import imgRegendWebUi from "../images/regend-logo-light.png";
+
+export type App = {
+  name: string;
+  copy: string;
+  badge?: string;
+  img?: ImageMetadata;
+  url: string;
+  outside: boolean;
+};
+
+export const main_apps: App[] = [
+  {
+    name: "Typing Game",
+    copy: "タイピングゲーム",
+    url: "/typing/",
+    outside: true,
+    badge:
+      "https://img.shields.io/github/deployments/yuma140902/typing/github-pages?logo=github&label=Deployment",
+  },
+  {
+    name: "Web Image Editor",
+    copy: "ブラウザ上で画像編集",
+    url: "/web-image-editor/",
+    badge:
+      "https://img.shields.io/github/deployments/yuma140902/web-image-editor/github-pages?logo=github&label=Deployment",
+    outside: true,
+    img: imgWebImageEditor,
+  },
+  {
+    name: "Diff Tool",
+    copy: "2つの文字列の差分を表示",
+    url: "/webtools/diff.html",
+    badge:
+      "https://img.shields.io/github/deployments/yuma140902/yuma140902.github.io/github-pages?logo=github&label=Deployment",
+    outside: false,
+    img: imgDiffTool,
+  },
+  {
+    name: "CSV to LaTeX",
+    copy: "エクセルの表をLaTeX形式に変換",
+    url: "/webtools/csv2tex.html",
+    badge:
+      "https://img.shields.io/github/deployments/yuma140902/yuma140902.github.io/github-pages?logo=github&label=Deployment",
+    outside: false,
+  },
+  {
+    name: "Regend WebUI",
+    copy: "正規表現をDFAに変換",
+    url: "/regend-webui/",
+    badge:
+      "https://img.shields.io/github/deployments/yuma140902/regend-webui/github-pages?logo=github&label=Deployment",
+    outside: true,
+    img: imgRegendWebUi,
+  },
+  {
+    name: "Sort Visualizer",
+    copy: "ソートアルゴリズムを可視化",
+    url: "/webtools/sort.html",
+    badge:
+      "https://img.shields.io/github/deployments/yuma140902/yuma140902.github.io/github-pages?logo=github&label=Deployment",
+    outside: false,
+  },
+  {
+    name: "sanmoku-next-js",
+    copy: "Next.jsで実装された三目並べ",
+    url: "/sanmoku-next-js/",
+    badge:
+      "https://img.shields.io/github/deployments/yuma140902/sanmoku-next-js/github-pages?logo=github&label=Deployment",
+    outside: true,
+  },
+  {
+    name: "gomoku-react",
+    copy: "Reactで実装された五目並べ",
+    url: "/gomoku-react/",
+    badge:
+      "https://img.shields.io/github/deployments/yuma140902/gomoku-react/github-pages?logo=github&label=Deployment",
+    outside: true,
+  },
+  {
+    name: "gomoku-blazor",
+    copy: "Blazorで実装された五目並べ",
+    url: "/gomoku-blazor/",
+    badge:
+      "https://img.shields.io/github/deployments/yuma140902/gomoku-blazor/github-pages?logo=github&label=Deployment",
+    outside: true,
+  },
+  {
+    name: "gomoku-elm",
+    copy: "Elmで実装された五目並べ",
+    url: "/gomoku-elm/",
+    badge:
+      "https://img.shields.io/github/deployments/yuma140902/gomoku-elm/github-pages?logo=github&label=Deployment",
+    outside: true,
+  },
+];
