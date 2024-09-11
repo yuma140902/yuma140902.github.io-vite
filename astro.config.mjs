e611fdcc5a071d9319cfc738b384704dbed1a5bb
yuma14 <yuma140902@gmail.com>
2024-09-12 00:14:16 +0900

    feat: add redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,10 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
+  redirects: {
+    "/deploys/": "/monitor/",
+    "/services/": "/monitor/",
+    "/works/forks/": "/monitor/",
+    "/webtools/": "/apps/",
+  },
 });
